d2139517a86a743307a073ac556cc779496ab7ef
Jacek <jacek.rudnicki@satlan.pl>
2018-03-24 11:45:56 +0100

    add location types & onChangeLocation method

---
+++ app/src/main/java/com/example/jacek/lifill/Data/Place.java
@@ -0,0 +1,9 @@
+package com.example.jacek.lifill.Data;
+
+/**
+ * Created by jacek on 24.03.18.
+ */
+
+public enum Place {
+    LivingRoom, Toilet, Kithen, Bathroom, Garage, Bedroom, Hall
+}
--- app/src/main/java/com/example/jacek/lifill/MainActivity.java
+++ app/src/main/java/com/example/jacek/lifill/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.jacek.lifill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.jacek.lifill.Data.Place;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Place currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onChangeLocation (Place sensorInfo){
+        if (sensorInfo==null){
+            Log.e("Location", "no location data received");
+            // TODO function to check if user leaved home (check wifi or gps info)
+        }
+
+        if (sensorInfo.equals(currentLocation)){
+            Log.e("Location", "received sensor info match current location " + sensorInfo );
+            return;
+        }
+
+        switch (sensorInfo) {
+            case Bathroom:
+                //code
+                break;
+            case Hall:
+                //code
+                break;
+            case Garage:
+                //code
+                break;
+            case Kithen:
+                //code
+                break;
+            case Toilet:
+                //code
+                break;
+            case Bedroom:
+                //code
+                break;
+            case LivingRoom:
+                //code
+                break;
+            default:
+                //code
+        }
+
+
     }
 }
